4d3958d597cae3c2e3782f0b5bfd899fb33557f6
Emilin Treesa Joseph <emilintresia02@gmail.com>
2023-01-26 21:05:51 +0530

    first commit

---
+++ react-redux/products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'When Wishes Bleed',
+        price: 140,
+        image:require('./assets/bleeds.jpeg')
+
+    },
+    {
+        id: 2,
+        name: 'Aladdin',
+        price: 195,
+        image:require('./assets/aladdin.jpg')
+    },
+    {
+        id: 3,
+        name: 'The Politics',
+        price: 220,
+        image:require('./assets/image3.jpg.jpeg')
+    },
+    {
+        id: 4,
+        name: 'Call Of Wild',
+        price: 180,
+        image:require('./assets/call.jpg')
+ 
+    },
+    {
+        id: 5,
+        name: 'Naomis Room',
+        price: 200,
+        image:require('./assets/pride.jpeg')
+    },
+
+]
